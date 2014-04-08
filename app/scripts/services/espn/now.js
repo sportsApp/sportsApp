8937fd28e3cd891aea151329ed838fb7a7e4b347
James Straub <james@porch.com>
2014-04-08 10:50:30 -0700

    Simple objects for news and now.

--- app/scripts/services/espn/now.js
+++ app/scripts/services/espn/now.js
@@ -1,7 +1,13 @@
+(function(window, angular, undefined) {'use strict';
 angular.module('espnAPI').
   service('espnNow', ['$resource', 'espnBase', function ($resource, espnBase){
-    var   url = espnBase.url() + 'now',
-      options = angular.extend({}, espnBase.options());
+    var     url = espnBase.url() + 'now',
+        options = angular.extend({}, espnBase.options());
 
-    return $resource(url, options);
+    return $resource(url, options, {
+          get: {method: 'JSONP', url: url + '/'},
+          top: {method: 'JSONP', url: url + '/top'},
+      popular: {method: 'JSONP', url: url + '/popular'},
+    });
   }]);
+})(window, window.angular);
