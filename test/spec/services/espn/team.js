0b834cf04462409216a7017551eb75b22a5a9dd8
Dave Lane <dlane@whitepages.com>
2014-04-11 21:42:25 -0700

    updated test to reflect correct url passed

--- test/spec/services/espn/team.js
+++ test/spec/services/espn/team.js
@@ -3,6 +3,7 @@ describe('Service: espnTeam', function () {
   var team,
       mocks,
       cache,
+      $routes,
       $httpBackend;
 
   // load the controller's module
@@ -11,6 +12,7 @@ describe('Service: espnTeam', function () {
 
   beforeEach(inject(function($injector) {
     $httpBackend = $injector.get('$httpBackend');
+    $routes = $injector.get('$routeParams');
     cache = $injector.get('espnCache');
     team = $injector.get('espnTeam');
     mocks = $injector.get('espnTeam.defaultResponse');
@@ -29,7 +31,7 @@ describe('Service: espnTeam', function () {
     beforeEach(function(){
       spyCB = jasmine.createSpy().andReturn(mocks);
       $httpBackend.expectJSONP(/v1\/sports\/football\/nfl\/teams\/26\?apiKey=/).respond(spyCB);
-      teamObj = team.get();
+      teamObj = team.get({sportType: 'football', league: 'nfl', teamId: '26'});
     });
 
     it('should return a promise', function(){
@@ -37,14 +39,6 @@ describe('Service: espnTeam', function () {
 
       expect(Object.getOwnPropertyNames(teamObj.$promise)).
         toEqual([ 'then', 'catch', 'finally' ]);
-    });
-
-    it('should cache', function(){
-      $httpBackend.flush();
-      return;
-      var before = cache.info().size;
-
-      expect(cache.info().size - before).toEqual(1);
     });
   });
 });
