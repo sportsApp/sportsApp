616eb8c802eb8b9398060952f4b9242cad17b383
James Straub <james@porch.com>
2014-04-13 18:42:55 -0700

    Added fetch from loacal storage (this should be a service maybe).

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -31,4 +31,18 @@ angular.module('sportsApp', [
   }).
   run(function(espnBase){
     espnBase.setAPIKey('3npcsdtfa9majpf6zr7a7dtz');
-  });
+  }).
+  run(function($rootScope){
+    $rootScope.settings = angular.fromJson(localStorage.getItem('sportsApp.settings')||'{}');
+    $rootScope.$watch('settings',function(oldVal, newVal){
+      localStorage.setItem('sportsApp.settings', angular.toJson(newVal, false));
+    });
+    $rootScope.$digest();
+  }).
+  constant('ICONS_LOCATOINS', {
+    'football': '/images/football.png'
+  }).
+  constant('DEFAULT_ICONS', [
+    '/images/medal.png',
+    '/images/track.png'
+  ]);
